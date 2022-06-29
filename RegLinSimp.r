99864e599c301b2547557c026e914ef4cb604701
blankhall98 <blankhall@ciencias.unam.mx>
2022-06-28 21:11:15 -0500

    Regresión Lineal Simple - 3 Versiones

--- RegLinSimp.r
+++ RegLinSimp.r
@@ -20,3 +20,34 @@ g1 <- ggplot(data = clients) +
   geom_boxplot(aes(x=Sex,y=Expenses))
 
 g1
+
+#grafica 2
+g2 <- ggplot(data = clients) + 
+  geom_point(aes(x=Age,y=Expenses,col=Sex,size=Salary))
+
+g2
+
+#grafica 3
+g3 <- g2 + geom_smooth(aes(x=Age,y=Expenses))
+
+g3
+
+#Regresion Lineal
+
+# modelo1
+modelo1 <- lm(data = clients, Expenses ~ Age)
+
+summary(modelo1)
+
+formula_m1 <- function(age){
+  return(-1843.6 + age*433.5)
+}
+
+# modelo 2
+modelo2 <- lm(data = clients, Expenses ~ Age + Sex)
+
+summary(modelo2)
+
+# modelo 3
+modelo3 <- lm(data = clients, Expenses ~ Age + Sex + Salary)
+summary(modelo3)
